a67c5166714e7b712667615d8ce74808be3928b0
Richard Danipog <richarddanipog@Richards-MBP-2.lan>
2023-08-09 10:38:55 +0300

    feat: add icons and fix style

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 import { Button, Card, Container } from "react-bootstrap";
 import Task from "./components/Task";
 import { ITask } from "./interfaces/task.interface";
+import { FileTextFill } from "react-bootstrap-icons";
 
 const App = () => {
   const [tasks, setTasks] = useState<ITask[]>([]);
@@ -18,14 +19,20 @@ const App = () => {
     <Container className="App">
       <Card className="wrapper-card">
         <Card.Body>
-          <h1>My ToDo</h1>
+          <h1>
+            <FileTextFill style={{ fill: "orange" }} size={35} />
+            My ToDo
+          </h1>
           <Container className="add-task">
             <input className="add-task-input" placeholder="Add New Task..." />
             <Button>Add</Button>
           </Container>
           <hr />
 
-          <Container className="p-0">
+          <Container>
+            <h6>Total ToDo's - ( {tasks.length} )</h6>
+          </Container>
+          <Container className="task-list p-0">
             {tasks.map((task) => (
               <Container key={task._id} className="task p-0">
                 <Task task={task} />
--- client/src/components/Task.tsx
+++ client/src/components/Task.tsx
@@ -1,22 +1,47 @@
 import { Card, Container } from "react-bootstrap";
 import { ITask } from "../interfaces/task.interface";
-import { useEffect } from "react";
+import { CheckSquareFill, XSquareFill } from "react-bootstrap-icons";
 
 type TaskProps = {
   task: ITask;
 };
 
 const Task = ({ task }: TaskProps): JSX.Element => {
+  const convertDateFormat = (date: string): string => {
+    const dateObject = new Date(date);
+
+    const longDateFormat: any = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+
+    const formattedDate = dateObject.toLocaleDateString(
+      undefined,
+      longDateFormat
+    );
+
+    return formattedDate;
+  };
+
   return (
     <>
       <Card className="mt-3">
-        <Card.Body>
-          <blockquote className="blockquote mb-0">
-            <p>{task.title}</p>
-            <footer className="blockquote-footer">
-              <cite title="Source Title">{task.description}</cite>
-            </footer>
-          </blockquote>
+        <Card.Body className="d-flex justify-content-between align-items-center">
+          <Container>
+            <blockquote className="blockquote mb-0">
+              <p>{task.title}</p>
+              <footer className="blockquote-footer">
+                <cite title="Source Title">{task.description}</cite>
+              </footer>
+            </blockquote>
+            <span>{convertDateFormat(task.createdAt)}</span>
+          </Container>
+          <Container className="task-actions">
+            <CheckSquareFill className="done-icon" size={30} />
+            <XSquareFill className="delete-icon" size={30} />
+          </Container>
         </Card.Body>
       </Card>
     </>
